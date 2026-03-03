a12baad6139b5f6b8bfa91afd53f94ec40dfdc3e
franciscormz <franciscoramirezvalerio@hotmail.com>
2026-03-03 13:27:42 -0600

    Ejemplo 1
    Ejemplo 1 de socketio

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('¡Hola Mundo!');
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// 👇 ESTA LÍNEA ES CLAVE
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    console.log('Cliente conectado');
+
+    socket.on('mensaje', (data) => {
+        io.emit('mensaje', data);
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Servidor en http://localhost:3000');
+});
